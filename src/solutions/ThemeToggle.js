12674ae8dd49a099b5d6b7a8465b6151915aa8cd
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-06 18:23:03 +0530

    toggle theme on button click

---
+++ src/solutions/ThemeToggle.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export const useTheme = () => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else setTheme("light");
+  };
+
+  return { theme, toggleTheme };
+};
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <div
+      style={{
+        height: "100vh",
+        transition: "0.3s ease-in",
+        backgroundColor: theme === "light" ? "white" : "black",
+      }}
+    >
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </div>
+  );
+};
+
+export default App;
