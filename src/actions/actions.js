095421cfd31caafd7317b27f7ade15e95ae2fa32
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-18 11:47:32 +0530

    created actions

---
+++ src/actions/actions.js
@@ -0,0 +1,19 @@
+import { redirect } from "react-router-dom";
+
+export async function userFormAction({ request }) {
+  const formData = await request.formData();
+
+  const data = {
+    firstName: formData.get("firstName"),
+    lastName: formData.get("lastName"),
+    dob: formData.get("dob"),
+    gender: formData.get("gender"),
+    qualification: formData.get("qualification"),
+    maritalStatus: formData.get("maritalStatus"),
+    address: formData.get("address"),
+  };
+
+  console.log("Form Submitted Data:", data);
+
+  return redirect("/thank-you"); // Redirect to a success page after submission
+}
