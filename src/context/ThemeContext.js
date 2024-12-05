81184b68d2eb24e075ecb575d3fe1b652e101329
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-05 15:38:38 +0530

    context api for theme added

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {" "}
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
