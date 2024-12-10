a46c79c970e961cde42dafebbc50687812f44fdc
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-10 12:25:44 +0530

    carousel handle click

--- src/Carousel/Carousel.jsx
+++ src/Carousel/Carousel.jsx
@@ -1,14 +1,33 @@
-import React from "react";
 import { useState } from "react";
 import "./carousel.css";
 
 const Carousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   const images = [
     "https://media.geeksforgeeks.org/wp-content/uploads/20230306120634/unnamed.jpg",
     "https://media.geeksforgeeks.org/wp-content/uploads/20230306120634/unnamed.jpg",
     "https://media.geeksforgeeks.org/wp-content/uploads/20230306120634/unnamed.jpg",
     "https://media.geeksforgeeks.org/wp-content/uploads/20230306120634/unnamed.jpg",
   ];
+
+  const handlePrevClick = () => {
+    if (activeIndex === 0) {
+      setActiveIndex((prev) => images.length - 1);
+    } else {
+      setActiveIndex((prev) => prev - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (activeIndex === images.length - 1) {
+      setActiveIndex((prev) => 0);
+    } else {
+      setActiveIndex((prev) => prev + 1);
+    }
+  };
+
+  console.log("activeIndex", activeIndex);
 
   return (
     <div className="componentContainer">
