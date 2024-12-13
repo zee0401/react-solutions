cf3e74002a3bc4802e500db09fa1171dae08e702
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-13 15:23:32 +0530

    add ewply input added

--- src/solutions/nested-comment/comments.jsx
+++ src/solutions/nested-comment/comments.jsx
@@ -1,12 +1,35 @@
 /* eslint-disable react/prop-types */
+import { useRef, useState } from "react";
+
 const Comments = ({ comment }) => {
+  const [getReply, setGetReply] = useState(false);
+
+  const inputRef = useRef();
+
+  const handleReply = () => {
+    setGetReply(true);
+    setTimeout(() => {
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, 0);
+  };
   return (
     <li key={comment.id} className="comment">
       {comment.body}
-      <button className="reply"> reply </button>
+      <button className="reply" onClick={handleReply}>
+        {" "}
+        reply{" "}
+      </button>
       <button className="reply"> delete </button>
 
       <div className="contain">
+        {getReply ? (
+          <div className="reply-input" inputRef>
+            <input ref={inputRef} />
+            <button className="btn"> add reply</button>
+          </div>
+        ) : null}
         {comment.children && comment.children.length > 0 ? (
           <ul>
             {comment.children.map((item) => (
