223c31ae04b038c52a5210d7cc209e25794c6e38
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-01-13 11:45:57 +0530

    app component

--- src/solutions/layoutComponents/app.jsx
+++ src/solutions/layoutComponents/app.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+const SplitScreen = ({ children, leftWeight = 1, rightWeight = 1 }) => {
+  const [left, right] = children;
 
-const app = () => {
-  return <div>app</div>;
+  const leftWidth = `${leftWeight}rem`;
+  const rightWidth = `${rightWeight}rem`;
+
+  return (
+    <section className="flex flex-1">
+      <div style={{ width: leftWidth }} className="p-4">
+        {left}
+      </div>
+      <div style={{ width: rightWidth }} className="p-4">
+        {right}
+      </div>
+    </section>
+  );
 };
 
-export default app;
+export default SplitScreen;
