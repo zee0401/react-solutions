df03e9a20fd4a1398d2b700c9b981be18ef604c8
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-01-13 11:44:16 +0530

    Sidebar Component

--- src/solutions/layoutComponents/components/Sidebar.jsx
+++ src/solutions/layoutComponents/components/Sidebar.jsx
@@ -1,7 +1,26 @@
-import React from "react";
-
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="bg-teal-500 h-full w-[10rem] text-white p-4">
+      <h2 className="text-xl">Dashboard</h2>
+      <ul className="mt-6 space-y-4">
+        <li>
+          <a href="#home" className="hover:text-gray-200">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#settings" className="hover:text-gray-200">
+            Settings
+          </a>
+        </li>
+        <li>
+          <a href="#profile" className="hover:text-gray-200">
+            Profile
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
