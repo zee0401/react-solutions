43f2897a3bd44d5739a4570552882a70643730bc
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-01-13 11:45:03 +0530

    SplitScreen Component

--- src/solutions/layoutComponents/components/SplitScreen.jsx
+++ src/solutions/layoutComponents/components/SplitScreen.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+/* eslint-disable react/prop-types */
 
-const SplitScreen = () => {
-  return <div>SplitScreen</div>;
+const SplitScreen = ({ children, leftWeight = 1, rightWeight = 1 }) => {
+  const [left, right] = children;
+
+  const leftWidth = `${leftWeight}rem`;
+  const rightWidth = `${rightWeight}rem`;
+
+  return (
+    <section className="flex flex-1">
+      <div style={{ width: leftWidth }} className="p-4">
+        {left}
+      </div>
+      <div style={{ width: rightWidth }} className="p-4">
+        {right}
+      </div>
+    </section>
+  );
 };
 
 export default SplitScreen;
