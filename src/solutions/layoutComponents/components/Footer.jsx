e51919836b7d6097705bb50e77ba5a537e9c1fbd
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-01-13 11:43:29 +0530

    Footer Component

--- src/solutions/layoutComponents/components/Footer.jsx
+++ src/solutions/layoutComponents/components/Footer.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-teal-800 text-white p-4 text-center">
+      <p>&copy; 2025 My Dashboard App</p>
+    </footer>
+  );
 };
 
 export default Footer;
