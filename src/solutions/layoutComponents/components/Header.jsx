dd17d083d7350a030eea3815f2ddeb854c120855
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-01-13 11:43:56 +0530

    Header Component

--- src/solutions/layoutComponents/components/Header.jsx
+++ src/solutions/layoutComponents/components/Header.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-teal-600  text-white p-4 flex justify-between items-center">
+      <h1 className="text-xl">My Dashboard</h1>
+      <div className="flex items-center space-x-4">
+        <button className="bg-teal-800 p-2 rounded-full">Profile</button>
+        <button className="bg-teal-800 p-2 rounded-full">Logout</button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
