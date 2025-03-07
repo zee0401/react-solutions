f1440808a1d2d1c1ecb9162ca80a9107850f4647
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-03-07 06:19:28 +0530

    accordian data

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import UserForm from "./UserForm";
-import { userFormAction } from "./actions";
-
-const router = createBrowserRouter([
-  {
-    path: "/submit-form",
-    element: <UserForm />,
-    action: userFormAction,
-  },
-  {
-    path: "/thank-you",
-    element: <div>Thank you for submitting the form!</div>,
-  },
-]);
+import React from "react";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+    return <div>App</div>;
 };
 
 export default App;
