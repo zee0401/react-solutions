702aa352dfd0f5ff07cafffe5fde0660833dadfd
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-03-11 16:51:35 +0530

    menu item

---
+++ src/solutions/tree-view/MenuItem.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
+import MenuList from "./MenuList";
+
+const MenuItem = ({ listItem }) => {
+    const [showChildren, setShowChildren] = useState({});
+
+    const handleClick = (id) => {
+        setShowChildren({
+            ...showChildren,
+            [id]: !showChildren[id],
+        });
+    };
+    console.log(showChildren);
+    return (
+        <div>
+            <ul>
+                <div className="menu-Item-2">
+                    <p>{listItem.label}</p>
+                    {listItem &&
+                    listItem.children &&
+                    listItem.children.length ? (
+                        <span onClick={() => handleClick(listItem.label)}>
+                            +
+                        </span>
+                    ) : null}
+                </div>
+                {listItem &&
+                listItem.children &&
+                showChildren[listItem.label] ? (
+                    <MenuList list={listItem.children} />
+                ) : null}
+            </ul>
+        </div>
+    );
+};
+
+export default MenuItem;
