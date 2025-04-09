95d188c87581969225f1ccb9fa14d9372a3adc36
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-04-10 01:02:07 +0530

    rmove from cart reducer

--- src/cart/cartSlice/cartSlice.js
+++ src/cart/cartSlice/cartSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = [];
 const cartSlice = createSlice({
     name: "cart",
     initialState,
@@ -8,9 +8,12 @@ const cartSlice = createSlice({
         addToCart(state, action) {
             state.push(action.payload);
         },
+        removeFromCart(state, action) {
+            return state.filter((item) => item.id !== action.payload);
+        },
     },
 });
 
-export const { addToCart } = cartSlice.action;
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
